e14044c93af66890c387a45aef4313f2f7a41365
unknown <steffen.winkler@gmx.net>
2009-09-22 21:57:54 +0200

    ignore .git*

---
+++ example/default_hash.pl
@@ -0,0 +1,38 @@
+#!perl
+
+use strict;
+use warnings;
+
+our $VERSION = 0;
+
+use Tie::Sub;
+
+my %data_hash = (
+    a => 'x',
+    b => 'y',
+    c => 'z',
+);
+
+tie my %default_hash, 'Tie::Sub', sub { ## no critic (Ties)
+    my $key = shift;
+
+    exists $data_hash{$key}
+        and return $data_hash{$key};
+
+    return 'default';
+};
+
+() = print <<"EOT";
+$default_hash{a}
+$default_hash{b}
+$default_hash{c}
+$default_hash{d}
+EOT
+
+# returns:
+# x
+# y
+# z
+# default
+
+# $Id$
